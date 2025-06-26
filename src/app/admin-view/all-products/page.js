47fb3f8873c9324bd73ad6e3608ce2d73d24a8a7
Durgesh Tiwari <twdurgesh226@gmail.com>
2025-06-26 20:34:56 +0530

    first commit

--- src/app/admin-view/all-products/page.js
+++ src/app/admin-view/all-products/page.js
@@ -1,11 +1,9 @@
+export const dynamic = "force-dynamic";
+
 import CommonListing from "@/components/CommonListing";
 import { getAllAdminProducts } from "@/services/product";
 
-
-
 export default async function AdminAllProducts() {
-
-  const allAdminProducts = await getAllAdminProducts()
-
-  return <CommonListing data={allAdminProducts && allAdminProducts.data}/>
+  const allAdminProducts = await getAllAdminProducts();
+  return <CommonListing data={allAdminProducts?.data || []} />;
 }
